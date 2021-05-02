30482113cb91668d8a32b92b498d541f33d57998
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-05-02 03:02:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 5000;
 global.appRoot = path.resolve(__dirname);
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
+    .use(express.json())
     .use(express.urlencoded())
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
